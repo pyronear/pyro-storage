1982c380171d66edce41dd2ccd32a75b05b6df47
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-12-20 19:36:58 +0100

    docs: Fixes Python headers

--- src/app/api/schemas.py
+++ src/app/api/schemas.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2022, Pyronear.
 
-# This program is licensed under the Apache License version 2.
-# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 
 from datetime import datetime
 from typing import List, Optional
@@ -15,7 +15,6 @@ from app.db.models import AccessType, MediaType
 class _CreatedAt(BaseModel):
     created_at: Optional[datetime] = None
 
-    @staticmethod
     @validator("created_at", pre=True, always=True)
     def default_ts_created(v):
         return v or datetime.utcnow()
